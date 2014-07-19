d24d9f78eba1f02c65c699151e375704c906a7de
catstiger <catstiger@gmail.com>
2014-07-19 11:19:52 +0800

    examples引入独立的xmodeler

---
+++ xmodeler/docs/examples/xmodeler/Icons.js
@@ -0,0 +1,22 @@
+var Icons = function() {
+	
+};
+/*
+ * 如果使用相对路径，那么必须以Icon.js所在的页面为标准，
+ * 而不能安装Icons.js为标准，毕竟，这不是css文件:(
+ */
+
+Icons.startEvent = 'xmodeler/images/start.png';
+Icons.endEvent = 'xmodeler/images/end.png';
+Icons.errorEndEvent = 'xmodeler/images/error-end.png';
+Icons.timerEvent = 'xmodeler/images/timer_event.png';
+Icons.exclusiveGateway = 'xmodeler/images/exclusive-gateway.png';
+Icons.parallelGateway = 'xmodeler/images/parallel_gateway.png';
+Icons.userTask = 'xmodeler/images/user.png';
+Icons.scriptTask = 'xmodeler/images/script.png';
+Icons.serviceTask = 'xmodeler/images/service.png';
+Icons.plus = 'xmodeler/images/plus.png';
+Icons.minus = 'xmodeler/images/minus.png';
+Icons.plug = 'xmodeler/images/plug_blue.gif';
+Icons.plugDisable = 'xmodeler/images/plug_disable.gif';
+
